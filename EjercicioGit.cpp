63364947312a9aea3c0c4db963f097a7feb908c4
cristian18-code <cristiangarzon180@gmail.com>
2020-04-26 17:17:11 -0500

    se ha añadido ejercicio en c++ para evidencia

---
+++ EjercicioGit.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	float numero, resultado;
+	
+	cout<<"Digite un numero para dividirlo entre 100: ";
+	cin>> numero;
+	
+	resultado = numero/100;
+	
+	cout<<"El resultado es: "<<resultado<<endl;
+	
+	return 0;
+}
